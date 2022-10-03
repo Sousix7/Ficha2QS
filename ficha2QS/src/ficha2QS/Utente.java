ea954f249cf67ed1a5faa2f26cfbf74444e5897a
Utilizador <Utilizador@10.180.5.181>
2022-10-03 14:44:39 +0100

    Versão1 - Parametros e construtores

---
+++ ficha2QS/src/ficha2QS/Utente.java
@@ -0,0 +1,60 @@
+package ficha2QS;
+
+public class Utente {
+	private String nome;
+	private String genero;
+	private int idade;
+	private float altura;
+	private int peso;
+	
+	
+	public Utente(String nome, String genero, int idade, float altura, int peso) 
+	{
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+}
